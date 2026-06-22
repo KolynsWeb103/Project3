7a39db2d78692b143e7df81a66f723fc2fa8815b
Kolyn090 <kolynlin@protonmail.com>
2026-06-21 23:37:21 -0400

    Commit before merge

--- client/src/components/GatheringSpot.jsx
+++ client/src/components/GatheringSpot.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import '../css/GatheringSpot.css'
+import item2icon from '../assets/item2icon.json'
+
+const iconImages = import.meta.glob('../assets/icons/*.png', {
+  eager: true,
+  import: 'default'
+})
+
+const itemIconMap = Object.fromEntries(
+  item2icon.map(item => [item.name, item.icon])
+)
 
 const GatheringSpot = (props) => {
+  const materials = Array.isArray(props.materials)
+    ? props.materials
+    : props.materials?.split(',').map(material => material.trim()) || []
+
+  const getIconImage = (material) => {
+    const iconName = itemIconMap[material]
+
+    if (!iconName) {
+      return null
+    }
+
+    return iconImages[`../assets/icons/${iconName}.png`]
+  }
+
   return (
     <article className='gathering-spot-information'>
       <div className='gathering-spot-information-overlay'>
@@ -20,9 +44,31 @@ const GatheringSpot = (props) => {
             <strong>Condition:</strong> {props.condition}
           </p>
 
-          <p>
-            <strong>Materials:</strong> {props.materials}
-          </p>
+          <div className='materials'>
+            <strong>Materials:</strong>
+
+            <div className='material-icons'>
+              {materials.map((material) => {
+                const iconImage = getIconImage(material)
+
+                return (
+                  <div className='material-icon' key={material}>
+                    {iconImage ? (
+                      <img
+                        src={iconImage}
+                        alt={material}
+                        title={material}
+                      />
+                    ) : (
+                      <span className='missing-icon'>?</span>
+                    )}
+
+                    <span>{material}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </article>
